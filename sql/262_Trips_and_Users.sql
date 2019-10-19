691b7ef29a1a70385982ee426620b4556f3a50db
SuchithKumar <kvsuchit@gmail.com>
2019-10-19 21:28:18 +0530

    Create 262_Trips_and_Users.sql

---
+++ sql/262_Trips_and_Users.sql
@@ -0,0 +1,12 @@
+select 
+    T.Request_at as Day,
+    round((sum(if(T.status!='completed',1,0))/sum(1)),2) as 'Cancellation Rate'
+from 
+    Trips T
+join
+    Users U ON T.Client_id=U.Users_Id
+where
+    U.Banned='No'
+    AND T.Request_at between '2013-10-01' and '2013-10-03'
+group by
+    T.Request_at
