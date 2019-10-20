3f85f03c32e947f1c9c623f6a627ced459d22b76
DataSecs <datasecs@outlook.de>
2019-10-20 14:08:23 +0200

    Refractors code for solution

--- java/65_Valid_Number.java
+++ java/65_Valid_Number.java
@@ -1,24 +1,6 @@
-public class Problem65ValidNumber {
+public class Solution {
 
-    public static void main(String[] args) {
-        String[] inputNumbers = {"0", " 0.1 ", "abc", "1 a", "2e10", " -90e3  ", " 1e", "e3", " 6e-1", " 99e2.5 ", "53.5e93", " --6 ", "-+3", "95a54e53", "+0", "-0", "e-1", "-e-1", "1e+10", "1+10", "1e+10+1", "1.e+101", "1e1.5", "1.e1.5", "1.e15", "1e-1", ".", "e", "+", "-", "9", "1", "1e.5", " ", "a", ".1", "1+", "1-", "3.", "0.001", ".e1", "+.8", "-.8", "-0.6e2", ".8+", ".8-", "6ee69", "6..69"};
-        boolean[] expectedStrings = {true, true, false, false, true, true, false, false, true, false, true, false, false, false, true, true, false, false, true, false, false, true, false, false, true, true, false, false, false, false, true, true, false, false, false, true, false, false, true, true, false, true, true, true, false, false, false, false};
-
-        for (int i = 0; i < inputNumbers.length; i++) {
-            boolean s2 = expectedStrings[i];
-            System.out.println("TEST: " + (i + 1));
-            System.out.println("Input: " + inputNumbers[i]);
-            System.out.println("Expected: " + s2);
-            boolean s1 = isNumber(inputNumbers[i]);
-            System.out.println("Output: " + s1);
-            if (!(s1 == s2)) {
-                System.out.println("CASE " + (i + 1) + " WRONG!");
-            }
-            System.out.println();
-        }
-    }
-
-    private static boolean isNumber(String s) {
+    public boolean isNumber(String s) {
         s = s.trim();
 
         int len = s.length();
