0d5ef7bf6eb86a8a5d595860e3bae4006194fb1f
Shravani Vadlamudi <vadlamudishravani20@gmail.com>
2019-10-30 15:19:16 +0530

    Added solution for problem #700 in cpp

---
+++ cpp/700_Search_in_BST.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (root == NULL)
+            return NULL;
+        if (root->val > val)
+            return searchBST(root->left,val);
+        else if (root->val < val)
+            return searchBST(root->right,val);
+        return root;
+    }
+};
